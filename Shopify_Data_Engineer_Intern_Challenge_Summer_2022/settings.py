0ec070524249c585bbcbb8e015afcd7690ca18b6
Rush <ruspatel1996@gmail.com>
2022-01-18 02:56:33 -0700

    using gunicorn for heroku and changed settings

--- Shopify_Data_Engineer_Intern_Challenge_Summer_2022/settings.py
+++ Shopify_Data_Engineer_Intern_Challenge_Summer_2022/settings.py
@@ -10,17 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import django_heroku
+
+import DATABASES as DATABASES
+import dj_database_url
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s*$wddvxu$0g4ak8*7n6z%o3uo4x7$(@&=r6(l-p8n4z6+opz9'
+# SECRET_KEY = 'django-insecure-s*$wddvxu$0g4ak8*7n6z%o3uo4x7$(@&=r6(l-p8n4z6+opz9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,8 +31,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CSRF_TRUSTED_ORIGINS = ['https://shopify-data-eng-intern.herokuapp.com']
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 
@@ -78,12 +79,7 @@ WSGI_APPLICATION = 'Shopify_Data_Engineer_Intern_Challenge_Summer_2022.wsgi.appl
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -125,5 +121,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 django_heroku.settings(locals())
