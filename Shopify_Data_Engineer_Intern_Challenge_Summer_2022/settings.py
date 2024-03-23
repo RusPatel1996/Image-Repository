1f536cb89728eb03450e4c6d9a3c2831c730c381
Rush <ruspatel1996@gmail.com>
2024-03-23 17:23:17 -0600

    Update settings.py

--- Shopify_Data_Engineer_Intern_Challenge_Summer_2022/settings.py
+++ Shopify_Data_Engineer_Intern_Challenge_Summer_2022/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s*$wddvxu$0g4ak8*7n6z%o3uo4x7$(@&=r6(l-p8n4z6+opz9'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
